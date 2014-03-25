e50ada61f95939b44fadbb3c708c3b01971dfa38
Kevin Ewing <kevin@Kevins-MacBook-Air.local>
2014-03-24 20:29:16 -0700

    now can force page refresh from server

--- public/javascripts/contentmanager.js
+++ public/javascripts/contentmanager.js
@@ -1,24 +1,26 @@
 (function() {
+
   var APP = window.APP || {};
 
   APP.contentManager = {
     pageCache: {},
-    loadPage: function(pageName, callback) {
+
+    loadPage: function(pageName, callback, force) {
       var self = this;
 
-      if (this.pageCache[pageName]) {
+      if (this.pageCache[pageName] && !force) {
         callback(this.pageCache[pageName].page);
       } else {
         $.ajax('/' + pageName).then(function(data) {
-          self.addPageToCache(pageName, data);
+          self.cachePage(pageName, data);
           callback(data);
         });
       }
     },
 
-    addPageToCache: function(pageName, data) {
+    cachePage: function(pageName, pageHtml) {
       this.pageCache[pageName] = {
-        page: data,
+        page: pageHtml,
         freshDate: Date.now()
       };
     }
