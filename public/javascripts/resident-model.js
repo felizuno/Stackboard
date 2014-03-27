db13987e02c5c4150610b01a2029cbea00ef664d
Kevin Ewing <notmyuwmail@gmail.com>
2014-03-26 19:06:43 -0700

    resident model file creation

---
+++ public/javascripts/resident-model.js
@@ -0,0 +1,12 @@
+(function() {
+  var APP = window.APP || {};
+
+  APP.Models = APP.Models || {};
+
+  APP.Models.Resident = Backbone.Model.extend({
+    initialize: function(config) {
+
+    }
+  });
+
+})();
